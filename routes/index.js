d3377a00ba6232ab8c25d4d5f116e2fad2f71076
FaithKimSheHer <136521596+FaithKimSheHer@users.noreply.github.com>
2023-07-30 11:15:15 -0400

    Create index.js

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+import usersRoutes from './users.js'; 
+import path from 'path';
+
+const constructorMethod = (app) => { 
+  app.use('/users', usersRoutes);
+  app.use('*', (req, res) => {res.redirect('/');}); 
+};
+
+export default constructorMethod;
