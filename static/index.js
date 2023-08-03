afeb429ef25421a3532de357e7f35ee30de95d83
FaithKimSheHer <136521596+FaithKimSheHer@users.noreply.github.com>
2023-08-03 18:39:45 -0400

    Delete static directory

--- static/index.js
+++
@@ -1,9 +0,0 @@
-import usersRoutes from './users.js'; 
-import path from 'path';
-
-const constructorMethod = (app) => { 
-  app.use('/users', usersRoutes);
-  app.use('*', (req, res) => {res.redirect('/users');}); 
-};
-
-export default constructorMethod;
