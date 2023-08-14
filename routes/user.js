a273eaba239898cb83f78c8e96eba6945187dbe8
FaithKimSheHer <136521596+FaithKimSheHer@users.noreply.github.com>
2023-08-14 13:07:09 -0400

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,16 @@
 import express from 'express';
 const router = express.Router();  
-import { usersFuncs } from '../data/index.js';
+import path from 'path';
+import { usersFuncs } from '../data/index.js'; 
 
-router
-    .route('/')
+router.route('/register')
     .get(async (req, res) => {
-        // res.sendFile(path.resolve('static/registration.html'));   
-        res.json({error: 'YOU SHOULD NOT BE HERE!'}); //let middleware handle this
-    });
-router
-    .route('/register')
-    .get(async (req, res) => {
-        res.status(200).render('register', {layout: 'user', title: 'Register'});
-    })
+        res.cookie('AuthCookie', req.session);
+        console.log("AuthCookie: ", req.session);
+        console.log("register/req.session.user:", req.session.user, " => redirecting to dashboard")
+        if(req.session.user)        return res.redirect('/'); 
+        else return res.sendFile(path.resolve('static/registration.html')); 
+    }) 
     .post(async (req, res) => {  
         const registrationForm = req.body;  
         
@@ -25,11 +23,12 @@ router
         if(emailAddress.substring(emailAddress.indexOf('@')), emailAddress.indexOf('.').length === 0)   throw "email address field error";
         if(emailAddress.substring(emailAddress.indexOf('.'), -1).length === 0) throw "email address field error";
         //console.log("Register: ", emailAddress);   
+
         const newUser = await usersFuncs.getByUserEmail(emailAddress);  
         //console.log("newUser: ", newUser, newUser!==null); 
         if(newUser!==null)     return res.render('registerError', {error: emailAddress}); 
         console.log("newUser: ", newUser, newUser!==null); 
-    
+ 
         try {   
             console.log('registrationForm', registrationForm);
             // Error handling  
@@ -98,71 +97,55 @@ router
                 if(state.length === 0)            throw "state field error";  
                 if(state.length !== 2)            throw "state field error"; 
                 if(/\d/.test(state))              throw "state field error";
-                if (/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(state))  throw "state field error"; 
-                //console.log("lastName", lastName);
-            console.log('state', state);
-        } catch (e) {
-            return res.status(200).render('registerError', {error: "Registration error"});
-        }        
-        //console.log("Register the user:\n", registrationForm); 
+                if (/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(state))  throw "state field error";  
+                //console.log('state', state);
+            } catch (e) {
+                return res.sendFile(path.resolve('static/registrationError.html')); 
+            }        
+            //console.log("Register the user:\n", registrationForm); 
         const user = await usersFuncs.addUser(registrationForm); 
-        console.log("Add user success! :", user);
-        res.status(200).render('register', {email: user.email}); 
-    }); //END: router.route('/').post(async (req, res)
+        console.log("Add user success:", user.email);
+        return res.sendFile(path.resolve('static/login.html'));
+    }); //END: router.route('/register') 
 
-router
-    .route('/login')
-    .get(async (req, res) => {
-        // res.sendFile(path.resolve('static/login.html'));   
-        res.status(200).render('login', {layout: 'user', title: 'Log In'});
-    })
+router.route('/login')
+    .get(async (req, res, next) => {  
+        res.cookie('AuthCookie', req.session);
+        console.log("AuthCookie: ", req.session);
+        console.log("login/req.session.user:", req.session.user, " => redirecting to login page")
+        if(req.session.user)        return res.redirect('/'); 
+        else return res.sendFile(path.resolve('static/login.html'));
+    }) 
     .post(async (req, res) => {  
-        const logInForm = req.body;  
-        
+        const logInForm = req.body;   
+
         let email = logInForm.registeredEmail;  
         let password = logInForm.registeredPassword;
+        
         // To find if email is valid => if it exists in mongodb
         if(email === undefined)                                   throw 'You must provide your email address'; 
         if(typeof email !== 'string')                             throw 'Email address must be a string';  
         else                                                      email = email.trim().toLowerCase();
         if(email.length === 0)                                    throw 'Email address cannot be an empty string or just spaces';   
         if(email.substring(0, email.indexOf('@')).length === 0)   throw 'Email address address error';  
-        if(email.substring(email.indexOf('@')), email.indexOf('.').length === 0)   throw 'Email address address error'; 
-        console.log("RegisteredUser:",email);    
+        if(email.substring(email.indexOf('@')), email.indexOf('.').length === 0)   throw 'Email address address error';  
 
-        const registeredUser = await usersFuncs.checkUser(email, password);   
+        const registeredUser = await usersFuncs.checkUser(email, password);       
+        if(!registeredUser)  return res.sendFile(path.resolve('static/loginError.html'));     
+        console.log("Registered User Found Success: ", registeredUser.email);
+        //console.log("RegisteredUser:",registeredUser);    
         
-        if(registeredUser === undefined)  return res.status(403).render('error', {layout: 'user', error: email});   
-
-        console.log("Registered User Found Success!");  
-        
-        return res.status(200).redirect("/");
-        // res.status(200).render('dash', {user: registeredUser}); 
-    }); //END: router.route('/logIn').post(async (req, res)
+        const currentTime = new Date().toString();  
+        req.session.user = { currentTime: currentTime, 
+                            email: registeredUser.email};
+        console.log("req.session.user", req.session.user)
+        res.status(200).render('dashboard', { userName: registeredUser.userName }); 
+    }); //END: router.route('/logIn')
 
 router.route('/logout').get(async (req, res) => {
     //code here for GET 
     res.cookie('AuthCookie', null); 
     req.session.destroy();
-    res.status(200).render('logout', {logout: "AuthCooki deleted successfully!"});
-});
-
-router.route('/:userName').get(async (req, res) => {  
-    
-    try {  
-        if (!req.params.userName)                     return res.status(400).json({error: "UserId does not exist"});
-        if (typeof req.params.userName !== 'string')  return res.status(400).json({error: "UserId Must be a string"});
-        req.params.userName = req.params.userName.trim();
-        if (req.params.userName.length === 0)         return res.status(400).json({error: "UserId can't be empty string"})    
-    } catch (e) {
-      return res.status(404).json({error: e});
-    }
-    try {
-        const userInfo = await usersFuncs.get(req.params.userName); 
-        res.status(200).json(userInfo);
-    } catch (e) {
-        res.status(404).json({error: e});
-    }
-});// END:router.route('/:userName').get(async (req, res)
-
+    return res.sendFile(path.resolve('static/logout.html'));
+}); 
 export default router; 
