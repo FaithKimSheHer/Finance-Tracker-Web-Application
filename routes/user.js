e386a73ff2ae63c3b3db075ae7dc82c801d27ece
FaithKimSheHer <136521596+FaithKimSheHer@users.noreply.github.com>
2023-08-16 17:27:05 -0400

    Update user.js
    discrepancy found and fixed

--- routes/user.js
+++ routes/user.js
@@ -134,14 +134,14 @@ router.route('/login')
 
         const registeredUser = await usersFuncs.checkUser(email, password);   
         
-        if(!registeredUser)  return res.status(403).render('loginError', {layout: 'user', error: email});   
+        if(!registeredUser)  res.status(200).render('loginError', {layout: 'user', error: email});   
  
         console.log("RegisteredUser:",registeredUser);    
-        console.log("Registered User Found Success: ", registeredUser.email); 
+        console.log("Registered User Found Success: ", email); 
         
         const currentTime = new Date().toString();  
         req.session.user = { currentTime: currentTime, 
-                             email: registeredUser.email};
+                             email: email};
         console.log("req.session.user", req.session.user)
         res.status(200).redirect("/"); 
     }); //END: router.route('/logIn') 
