9ba0ab5651877b60bf63e04f85beae37b2a5d710
Tythla <geeksoap@outlook.com>
2023-08-17 17:03:27 -0400

    Add transactions post route

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,26 +1,36 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import { transactFuns } from '../data/index.js';
+import { transactFuns } from "../data/index.js";
 
-router.route('/summary').get(async (req, res) => {
+router.route("/summary").get(async (req, res) => {
   try {
     const data = await transactFuns.getAllTransactions();
-    return res.render('summary', {summary: data})
+    return res.render("summary", { summary: data });
   } catch (error) {
-    return res.status(404).render('error', {errorMessage: "Summary page not found."})
+    return res
+      .status(404)
+      .render("error", { errorMessage: "Summary page not found." });
   }
-
 });
 
-router.route('/transaction_summary/:id').get(async (req, res) => {
+router.route("/transaction_summary/:id").get(async (req, res) => {
   const id = req.body.id;
   try {
-    return res.render('/transaction_summary', {transaction: transaction})
-  } catch (error) {
-
-  }
+    return res.render("/transaction_summary", { transaction: transaction });
+  } catch (error) {}
   const transaction = await transactFuns(id);
+});
 
-})
+router.route("/add_transaction").post(async (req, res) => {
+  try {
+    const newTransaction = req.body;
+    await transactFuns.addTransaction(newTransaction);
+    return res.redirect("/summary");
+  } catch (error) {
+    return res
+      .status(500)
+      .render("error", { errorMessage: "Error adding transaction." });
+  }
+});
 
-export default router
+export default router;
