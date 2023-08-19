c9aeaaee6464718f85074c365a8de701d409cdf6
iscii <issac.zheng9869@gmail.com>
2023-08-19 15:25:26 -0400

    Remove registration.css, added notice for transactions, removed alerts for transactions, fixed transactions handlebars html

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -6,37 +6,41 @@
 const updateForm = document.getElementById("updateForm"); // For now, assumed form ID to be 'updateTransactionForm'
 //const createTransactionForm = document.getElementById("createTransactionForm");
 
-
+console.log('hei')
 
 // Event listeners for update.handlebars form:
 updateForm.addEventListener("submit", async (event) => {
-    if (!validateUpdateForm()) { // If form validation fails, prevent form submission
+    if(!validateUpdateForm()){
         event.preventDefault();
     }
-    updateForm.reset();
 });
 
-// Client side validations for tranasaction update.handlebars
+// Client side validations for transaction update.handlebars
 function validateUpdateForm() {
+    const noticeEl = document.querySelector(".notice");
+
     // Get values from the update form
     const amount = parseFloat(document.getElementById("transactionAmount").value);
     const date = document.getElementById("transactionDate").value.trim();
     const category = document.getElementById("updateSelector").value.trim();
-    console.log(typeof amount);
+
+    // Check if any (of the required) values are empty, if so noticeEl.innerHTML = user
+    if (!amount || !date || !category) {
+        noticeEl.innerHTML = "Please fill out all fields";
+        return false;
+    }
 
     // Check type of values
-    if (isNaN(amount) || amount <= 0) { // Validate amount is a number
-        alert("Please enter a valid amount");
-        return false;
-    }
-
-    if (typeof date !== 'string') {
-        alert("Please enter a valid date"); // Validate date is a string
-        return false;
-    }
-
     if (typeof category !== 'string') {
-        alert("Please enter a valid category"); // Validate category is a string
+        noticeEl.innerHTML = "Please enter a valid category"; // Validate category is a string
+        return false;
+    }
+    if (isNaN(amount) || amount <= 0) { // Validate amount is a number
+        noticeEl.innerHTML = "Please enter a valid amount";
+        return false;
+    }
+    if (typeof date !== 'string') {
+        noticeEl.innerHTML = "Please enter a valid date"; // Validate date is a string
         return false;
     }
 
@@ -49,41 +53,35 @@ function validateUpdateForm() {
     // Check types of optional field values
     if (transactionInfo || receiptFilename || pathOfFilename || userComments) {
         if (typeof transactionInfo !== 'string') {
-            alert("Please enter valid transaction info"); // Validate transactionInfo is a string
+            noticeEl.innerHTML = "Please enter valid transaction info"; // Validate transactionInfo is a string
             return false;
         }
 
         if (typeof receiptFilename !== 'string') {
-            alert("Please enter a valid receiptFilename"); // Validate receiptFilename is a string
+            noticeEl.innerHTML = "Please enter a valid receiptFilename"; // Validate receiptFilename is a string
             return false;
         }
 
         if (typeof pathOfFilename !== 'string') {
-            alert("Please enter a valid pathOfFilename"); // Validate pathOfFilename is a string
+            noticeEl.innerHTML = "Please enter a valid pathOfFilename"; // Validate pathOfFilename is a string
             return false;
         }
 
         if (typeof userComments !== 'string') {
-            alert("Please enter a valid comment"); // Validate userComments is a string
+            noticeEl.innerHTML = "Please enter a valid comment"; // Validate userComments is a string
             return false;
         }
     }
 
-    // Check if any (of the required) values are empty, if so alert user
-    if (!amount || !date || !category) {
-        alert("Please fill out all fields");
-        return false;
-    }
-
     // Validate date is in correct format
     if (!date.match(/^\d{4}-\d{2}-\d{2}$/)) {
-        alert("Please enter a valid date");
+        noticeEl.innerHTML = "Please enter a valid date";
         return false;
     }
 
     // Validate category is one of the 5 options
     if (!['Income', 'Savings', 'Expenditure', 'Retirement', 'Investment'].includes(category)) {
-        alert("Invalid category value");
+        noticeEl.innerHTML = "Invalid category value";
         return false;
     }
 
